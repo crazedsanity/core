cb767c9ea899143d8e854904b2d9c7725cca6b87
Dan Falconer <github@crazedsanity.com>
2017-08-15 09:27:28 -0500

    Changes for PHPUnit on Travis-CI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,8 +10,7 @@ if(file_exists(__DIR__ .'/../vendor/autoload.php')) {
 	require_once(__DIR__ .'/../vendor/autoload.php');
 }
 // backward compatibility
-if (!class_exists('\PHPUnit\Framework\TestCase') &&
-    class_exists('\PHPUnit_Framework_TestCase')) {
+if (class_exists('\PHPUnit\Framework\TestCase') && !class_exists('\PHPUnit_Framework_TestCase')) {
     class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
 }
 
